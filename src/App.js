6cd957217fb02f2f2ff7ab8fb13814c15ec96c7a
Leandro <lterrado@gmail.com>
2019-11-12 20:53:46 -0300

    The begginig

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import CardList from './CardList'
+import SearchBox from './SearchBox'
+import {robots} from './robots';
+
+
+class App extends Component {
+        constructor(){
+                super();
+                this.state = {
+                        robots: robots,
+                        searchField: "",
+                };
+        }
+
+        OnSearchChange = (event) => {
+                this.setState({searchField: event.target.value});
+        }
+
+        render(){
+                const filteredRobots = this.state.robots.filter(robot =>{
+                        return robot.name.toLocaleLowerCase().includes(this.state.searchField.toLocaleLowerCase());
+                })
+                return(
+                        <div className="tc">
+                                <h1>Robofriends</h1>
+                                <SearchBox searchChange={this.OnSearchChange} />
+                                <CardList robots={filteredRobots} />
+                        </div>
+                );
+        }
+}
+
+
+
+export default App;
