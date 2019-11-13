c370e68b9e689f11575ce81e173bb29bafa239f7
Leandro <lterrado@gmail.com>
2019-11-13 14:26:41 -0300

    Updates

---
+++ precache-manifest.a0460d1690369ea1cdee986f1ad0f092.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55a9e9118229b82cac60ea79e17f0b65",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "eb2e83d34c498388c016",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "059ea4dbc1410672a2fc",
+    "url": "/robofriends/static/css/main.70e54048.chunk.css"
+  },
+  {
+    "revision": "eb2e83d34c498388c016",
+    "url": "/robofriends/static/js/2.591f2f3b.chunk.js"
+  },
+  {
+    "revision": "059ea4dbc1410672a2fc",
+    "url": "/robofriends/static/js/main.a0ee5152.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  }
+]);
