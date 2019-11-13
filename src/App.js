77d1f425ed9a7ec657c5502c8dd678a03098d5b8
Leandro <lterrado@gmail.com>
2019-11-12 22:45:52 -0300

    agregue API REST y loading

--- src/App.js
+++ src/App.js
@@ -9,12 +9,21 @@ class App extends Component {
         constructor(){
                 super();
                 this.state = {
-                        robots: robots,
+                        robots: [],
                         searchField: "",
                 };
+         }
+
+        componentDidMount(){
+                this.setState({robots:robots});
+
         }
 
         OnSearchChange = (event) => {
+                fetch("https://jsonplaceholder.typicode.com/users")
+                .then(response => response.json())
+                .then(users => this.setState({robots: users}));
+                
                 this.setState({searchField: event.target.value});
         }
 
@@ -22,13 +31,20 @@ class App extends Component {
                 const filteredRobots = this.state.robots.filter(robot =>{
                         return robot.name.toLocaleLowerCase().includes(this.state.searchField.toLocaleLowerCase());
                 })
-                return(
-                        <div className="tc">
-                                <h1 className = "f1"> robofriends </h1>
-                                <SearchBox searchChange={this.OnSearchChange} />
-                                <CardList robots={filteredRobots} />
-                        </div>
-                );
+                if (this.state.robots.length === 0){
+                        return <h1>Loading</h1>;
+                }
+                else{
+                        return(
+                                <div className="tc">
+                                        <h1 className = "f1"> robofriends </h1>
+                                        <SearchBox searchChange={this.OnSearchChange} />
+                                        <CardList robots={filteredRobots} />
+                                </div>
+                        );
+                }
+                
+
         }
 }
 
